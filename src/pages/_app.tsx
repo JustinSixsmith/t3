c6c1ca481a4ef5e1feb6d7d78557501c63df2030
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-24 23:06:59 -0500

    Installs Clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import { SignedIn } from "@clerk/nextjs";
+import { SignedOut } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider>
+      <SignedOut>
+        <Component {...pageProps} />
+      </SignedOut>
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
