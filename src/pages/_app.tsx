f47f9ec73fe4d61f61c2d9dadced147e00200d67
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-25 22:35:47 -0500

    Creates Post router

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,12 @@
+import { ClerkProvider, SignedOut } from "@clerk/nextjs";
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { UserButton } from "@clerk/nextjs";
-import { SignedIn } from "@clerk/nextjs";
-import { SignedOut } from "@clerk/nextjs";
-import { SignIn } from "@clerk/nextjs";
+import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider>
-      <SignedOut>
-        <Component {...pageProps} />
-      </SignedOut>
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 };
